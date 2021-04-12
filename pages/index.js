51e225da9515c4438a0e11a7bf87352fd68c9a01
JuanJoseJ <jjj.1998@hotmail.com>
2021-04-11 23:20:35 -0500

    Added support for enviroment in the apis

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function CurriculumVitae({ profileData, studeisData, workData, pr
     <>
       <Head>
         <title>Curriculum Vitae</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
